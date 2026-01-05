953ada7abfe493b5d9b4b0f24fad41619a8295f0
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2026-01-06 01:10:08 +0600

    Add variable declaration and basic operations examples

---
+++ 1. Basics/variable.js
@@ -0,0 +1,18 @@
+// Problem 1: Declare a variable and print it
+let name = "Milu";
+console.log(name);
+
+// Problem 2: Store two numbers and print their sum
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+// Problem 3: Swap two variables
+let x = 5;
+let y = 10;
+
+let temp = x;
+x = y;
+y = temp;
+
+console.log(x, y);
