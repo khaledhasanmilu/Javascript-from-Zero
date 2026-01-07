e09feec1c47345fd5e36f22e6355e589c81173da
Khaled Hasan Milu <118599823+khaledhasanmilu@users.noreply.github.com>
2026-01-08 01:34:12 +0600

    Add files via upload

---
+++ 03-Loops/forOfLoop.js
@@ -0,0 +1,16 @@
+const cars = ["BMW", "Volvo", "Mini"];
+
+for (let x of cars) {
+  console.log(x);
+}
+
+const fruits = ["Apple", "Banana", "Mango"];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+} 
+const num = [1,2,3,4,5];
+
+for(let number of num){
+  console.log(number);
+} 
---
+++ 03-Loops/for_In_loop.js
@@ -0,0 +1,8 @@
+object = {
+  name:"Khaled",
+  age:24,
+  country:"Bangladesh"
+};
+for(let key in object) {
+  console.log(key + ": " + object[key]);
+}
